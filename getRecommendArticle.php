69df1dc7477716b0fe9aa5c1015ca430f7ad5bf2
david21985 <309786382@qq.com>
2016-04-25 16:38:27 +0800

    files

---
+++ getRecommendArticle.php
@@ -0,0 +1,13 @@
+<?php
+echo [{"id":1000001,"title":"表白1","subTitle":"投其所好，打造别出心裁的定制求婚","thumbUrl":"http://img0.ph.126.net/S3cj7T-h4c_DjBlLlRllBw==/1052434937938576722.jpg"},
+ {"id":1000002,"title":"表白2","subTitle":"没钱也能任性，花最少的钱做最特别的圆公主梦","thumbUrl":"http://img2.ph.126.net/7FMn8vPP9dLQXiMzXSb1mQ==/6619356867351834416.jpg"},
+ {"id":1000003,"title":"表白3","subTitle":"最近大热的撕名牌大战怎么举行才最好玩？详细攻略逐个击破","thumbUrl":"http://img2.ph.126.net/MkH-kVdsWqW8edG9NO9LpQ==/6619460221444845416.jpg"},
+ {"id":1000004,"title":"旅游","subTitle":"fasfsafa","thumbUrl":"http://images.cnitblog.com/i/450136/201406/201931451766603.png"},
+ {"id":1000005,"title":"归感","subTitle":"fasffasfsasafa","thumbUrl":"http://wenwen.soso.com/p/20111210/20111210121531-1511464597.jpg"},
+ {"id":1000006,"title":"iiodjgoihsdoighio发了合法I类分hi按键","subTitle":"f发覅爱哦福建海鸥飞还哦好asffasfsasafa","thumbUrl":"http://images.cnitblog.com/i/450136/201406/201931451766603.png"},
+ {"id":1000007,"title":"旅游","subTitle":"fasfsafa","thumbUrl":"http://images.cnitblog.com/i/450136/201406/201931451766603.png"},
+ {"id":1000008,"title":"归感","subTitle":"fasffasfsasafa","thumbUrl":"http://wenwen.soso.com/p/20111210/20111210121531-1511464597.jpg"},
+ {"id":1000009,"title":"iiodjgoihsdoighio发了合法I类分hi按键","subTitle":"f发覅爱哦福建海鸥飞还哦好asffasfsasafa","thumbUrl":"http://images.cnitblog.com/i/450136/201406/201931451766603.png"},
+ {"id":1000010,"title":"旅游","subTitle":"fasfsafa","thumbUrl":"http://images.cnitblog.com/i/450136/201406/201931451766603.png"},
+ {"id":1000011,"title":"归感","subTitle":"fasffasfsasafa","thumbUrl":"http://wenwen.soso.com/p/20111210/20111210121531-1511464597.jpg"}]
+?>
